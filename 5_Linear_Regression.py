2d758e538fb772c295f237489de07abe93da5c28
Ricardo Diaz <ricardo.andres.diaz@correounivalle.edu.co>
2021-10-06 10:54:53 -0500

    Added comments for Linear Regression

--- 5_Linear_Regression.py
+++ 5_Linear_Regression.py
@@ -1,12 +1,14 @@
-from torch import nn
 import torch
+from torch import nn
 from torch import tensor
 
 x_data = tensor([[1.0], [2.0], [3.0]])
 y_data = tensor([[2.0], [4.0], [6.0]])
+hours_of_study = 4.0
 
+# Steps to create a Neural Network
 # Step 1: Design the Model using classes
-# Step 2: Define and loss function and optimizer
+# Step 2: Define a loss function and optimizer
 # Step 3: Train your model
 
 class Model(nn.Module):
@@ -29,14 +31,16 @@ class Model(nn.Module):
         return y_pred
 
 
-# our model
+# Our model
 model = Model()
 
 # Construct our loss function and an Optimizer. The call to model.parameters()
 # in the SGD constructor will contain the learnable parameters of the two
 # nn.Linear modules which are members of the model.
-criterion = torch.nn.MSELoss(reduction='sum')
+loss_function = torch.nn.MSELoss(reduction='sum')
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+# Other optimizers: torch.optim.Adagrad, torch.optim.Adam, torch.optim.Adamax, torch.optim.ASGD, torch.optim.LBFGS, torch.optim.RMSprop, torch.optim.Rprop, torch.optim.SGD
 
 # Training loop
 for epoch in range(500):
@@ -44,7 +48,8 @@ for epoch in range(500):
     y_pred = model(x_data)
 
     # 2) Compute and print loss
-    loss = criterion(y_pred, y_data)
+    # Both y_pred and y_data are tensors
+    loss = loss_function(y_pred, y_data)
     print(f'Epoch: {epoch} | Loss: {loss.item()} ')
 
     # Zero gradients, perform a backward pass, and update the weights.
@@ -53,7 +58,7 @@ for epoch in range(500):
     optimizer.step()
 
 
-# After training
-hour_var = tensor([[4.0]])
+# What we are trying to do is predict the score if we have x hours of study
+hour_var = tensor([[hours_of_study]])
 y_pred = model(hour_var)
-print("Prediction (after training)",  4, model(hour_var).data[0][0].item())
+print("Prediction (after training)",  hours_of_study, model(hour_var).data[0][0].item())
