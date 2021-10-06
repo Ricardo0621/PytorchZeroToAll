7ee93f4f356ffa07e77de29ff7c24eb9946455d6
Ricardo Diaz <ricardo.andres.diaz@correounivalle.edu.co>
2021-10-06 12:11:53 -0500

    More useful comments

--- 5_Linear_Regression.py
+++ 5_Linear_Regression.py
@@ -2,8 +2,12 @@ import torch
 from torch import nn
 from torch import tensor
 
+# x_data is the number of hours studied
 x_data = tensor([[1.0], [2.0], [3.0]])
+# y_data is the number of points obtained
 y_data = tensor([[2.0], [4.0], [6.0]])
+# e.g: 1 hour of study -> 2 points. 2 hours of study -> 4 points usw
+# hours_of_study is the parameter we pass on. What we want to predict is our score
 hours_of_study = 4.0
 
 # Steps to create a Neural Network
@@ -34,12 +38,13 @@ class Model(nn.Module):
 # Our model
 model = Model()
 
-# Construct our loss function and an Optimizer. The call to model.parameters()
-# in the SGD constructor will contain the learnable parameters of the two
+# Construct our loss function and an Optimizer. 
+# The call to model.parameters() in the SGD constructor will contain the learnable parameters of the two
 # nn.Linear modules which are members of the model.
 loss_function = torch.nn.MSELoss(reduction='sum')
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-
+for name in model.named_parameters():
+    print (name)
 # Other optimizers: torch.optim.Adagrad, torch.optim.Adam, torch.optim.Adamax, torch.optim.ASGD, torch.optim.LBFGS, torch.optim.RMSprop, torch.optim.Rprop, torch.optim.SGD
 
 # Training loop
@@ -50,15 +55,18 @@ for epoch in range(500):
     # 2) Compute and print loss
     # Both y_pred and y_data are tensors
     loss = loss_function(y_pred, y_data)
-    print(f'Epoch: {epoch} | Loss: {loss.item()} ')
+    # print(f'Epoch: {epoch} | Loss: {loss.item()} ')
 
-    # Zero gradients, perform a backward pass, and update the weights.
+    # Inside the training loop, optimization happens in three steps:
+    # Call optimizer.zero_grad() to reset the gradients of model parameters. Gradients by default add up; to prevent double-counting, we explicitly zero them at each iteration.
     optimizer.zero_grad()
+    # Backpropagate the prediction loss with a call to loss.backwards(). PyTorch deposits the gradients of the loss w.r.t. each parameter.
     loss.backward()
+    # Once we have our gradients, we call optimizer.step() to adjust the parameters by the gradients collected in the backward pass.
     optimizer.step()
 
 
-# What we are trying to do is predict the score if we have x hours of study
+# What we are trying to do is predict the score or points we will get if we study hours_of_study
 hour_var = tensor([[hours_of_study]])
 y_pred = model(hour_var)
-print("Prediction (after training)",  hours_of_study, model(hour_var).data[0][0].item())
+print(f'If we study {hours_of_study} hours we will get a score of {model(hour_var).data[0][0].item()}')
